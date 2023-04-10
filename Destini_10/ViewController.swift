cbe02c9dc8a954930f8ea702921e8b7ad9822573
Larisa Teregulova <larisa.andreeva.1995@mail.ru>
2023-04-10 14:27:09 +0300

    Initial Commit

---
+++ Destini_10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini_10
+//
+//  Created by Лариса Терегулова on 10.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
