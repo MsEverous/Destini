77475ddca64b2666c334432f31c7d5fabb0bdde3
Larisa Teregulova <larisa.andreeva.1995@mail.ru>
2023-04-10 22:37:30 +0300

    change story

--- Destini_10/Model/Story.swift
+++ Destini_10/Model/Story.swift
@@ -11,10 +11,14 @@ struct Story {
     var title: String
     var choice1: String
     var choice2: String
+    var choice1Destination: Int
+    var choice2Destination: Int
     
-    init(t: String, c1: String, c2: String) {
+    init(t: String, c1: String, d1: Int, c2: String, d2: Int) {
         title = t
         choice1 = c1
         choice2 = c2
+        choice1Destination = d1
+        choice2Destination = d2
     }
 }
