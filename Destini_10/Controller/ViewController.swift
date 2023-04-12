4c70804b5899b472dfad2d851e83beda80385305
Larisa Teregulova <larisa.andreeva.1995@mail.ru>
2023-04-12 10:40:11 +0300

    Done

--- Destini_10/Controller/ViewController.swift
+++ Destini_10/Controller/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         storyLabel.text = storyBrain.story[0].title
-        
         settingsButton()
         stepChoice()
     }
---
+++ README.md
@@ -0,0 +1,5 @@
+## Destini 
+
+# App Screen
+![Screen](Documentation/giphy.gif)
+
